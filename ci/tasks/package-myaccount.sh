7d7fdb90fb84fed1a42f9e008e618a2aaba35ef0
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-03 14:43:07 +0100

    test maven cache

--- ci/tasks/package-myaccount.sh
+++ ci/tasks/package-myaccount.sh
@@ -2,8 +2,16 @@
 
 set -e -u -x
 
-cd source-code/person-service
-../mvnw clean install
+export BUILD_FOLDER=$( pwd )
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${BUILD_FOLDER}/maven"
+
+echo "Generating the /.m2 symbolic link for /maven cache"
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+#cd source-code/person-service
+#../mvnw clean install
 
 cd ../myaccount-client
 ../mvnw clean install
