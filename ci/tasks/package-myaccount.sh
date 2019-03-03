1fae27822871a9ea4bc03ee633e3f0a5eb60a53f
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-03 15:29:41 +0100

    remove caching on the myaccount

--- ci/tasks/package-myaccount.sh
+++ ci/tasks/package-myaccount.sh
@@ -2,16 +2,16 @@
 
 set -e -u -x
 
-export BUILD_FOLDER=$( pwd )
+#export BUILD_FOLDER=$( pwd )
+#
+#M2_HOME="${HOME}/.m2"
+#M2_CACHE="${BUILD_FOLDER}/maven"
+#
+#echo "Generating the /.m2 symbolic link for /maven cache"
+#[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 
-M2_HOME="${HOME}/.m2"
-M2_CACHE="${BUILD_FOLDER}/maven"
-
-echo "Generating the /.m2 symbolic link for /maven cache"
-[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
-
-#cd source-code/person-service
-#../mvnw clean install
+cd source-code/person-service
+../mvnw clean install
 
 cd source-code/myaccount-client
 ../mvnw clean install
